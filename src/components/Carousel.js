148638fdb60408c67106478f513a22c468b50886
ETLDevs <etlbdevs@gmail.com>
2023-05-08 18:44:32 +0300

    aranged the carousel imag and buttons

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,27 +1,38 @@
 // import { useContext } from "react";
 // import { PhotoContext } from "../contexts/PhotoContext";
 
-
 const Carousel = (props) => {
-    const {target, setTarget} = props
+  const { target, setTarget } = props;
   return (
-    
     <>
-   <div className="darkBg" onClick={() => setTarget(null)}></div>
-      
+      <div className="darkBg" onClick={() => setTarget(null)}></div>
+
       <div className="carousel">
-      <button onClick={() => {
-        setTarget(target.previousSibling)
-        if(!target.previousSibling) setTarget(target.parentElement.lastChild)
-        }}><i className="fa-solid fa-arrow-left fa-beat"></i></button>
-        <img src={target.firstChild.src} alt={target.firstChild.alt} />
-        <button  onClick={() => {
-          setTarget(target.nextSibling)
-          if(!target.nextSibling) setTarget(target.parentElement.firstChild)
-          }}><i className="fa-solid fa-arrow-right fa-beat"></i></button>
-      
+      <div className="btnsContainer">
+            <button
+              onClick={() => {
+                setTarget(target.previousSibling);
+                if (!target.previousSibling)
+                  setTarget(target.parentElement.lastChild);
+              }}
+            >
+              <i className="fa-solid fa-arrow-left fa-beat"></i>
+            </button>
+            <button
+              onClick={() => {
+                setTarget(target.nextSibling);
+                if (!target.nextSibling)
+                  setTarget(target.parentElement.firstChild);
+              }}
+            >
+              <i className="fa-solid fa-arrow-right fa-beat"></i>
+            </button>
+          </div>
+        <div className="carouselImg">
+          <img src={target.firstChild.src} alt={target.firstChild.alt} />
+          <div className="carouselImgDetails">{target.firstChild.alt}</div>
+        </div>
       </div>
-      
     </>
   );
 };
