df6195102f85fc18338d4069ace1f4288de20028
ETLDevs <etlbdevs@gmail.com>
2023-05-16 18:03:13 +0300

    change of LoadMore

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -5,48 +5,45 @@ import LoadMore from "./LoadMore";
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 import Carousel from "./Carousel";
 
-const Gallery = ({query}) => {
-const {fetchPhotos, photos} = useContext(PhotoContext);
-const {searchValue} = useParams()
-const [pageNum, setPageNum] = useState(1);
-const [target,  setTarget] = useState('')
-const keyword = query ? query : searchValue 
-useEffect(() => {
-  setPageNum(1)
-}, [keyword])
-useEffect(() => {
-fetchPhotos(keyword, pageNum) // eslint-disable-next-line 
-}, [pageNum, keyword])
+const Gallery = ({ query }) => {
+  const { fetchPhotos, photos } = useContext(PhotoContext);
+  const { searchValue } = useParams();
+  const [target, setTarget] = useState("");
+  const keyword = query ? query : searchValue;
+  useEffect(() => {
+    fetchPhotos(keyword, 1);
+  }, [keyword, fetchPhotos]);
 
-
-  return photos.length  ? (
+  return photos.length ? (
     <div className="galleryContainer">
-    <h2>results of {keyword}</h2>
-    <h3>{photos.length} images</h3>
-    <div className="gallery"
-    onClick={ e => {
-     if(e.target.classList.contains('galleryImg')) {
-     setTarget(e.target.parentElement)
-    //  document.querySelector('.carouselModal').classList.remove('hidden')
-     }
-    }}
-    >
-      {
-      photos.map((photo) => {
+      <h2>results of {keyword}</h2>
+      <h3>{photos.length} images</h3>
+      <div
+        className="gallery"
+        onClick={(e) => {
+          if (e.target.classList.contains("galleryImg")) {
+            setTarget(e.target.parentElement);
+          }
+        }}
+      >
+        {photos.map((photo) => {
           return (
-            
-            <Image  title={photo.tags} url={photo.webformatURL} key={photo.id} setTarget={setTarget}/>
+            <Image
+              title={photo.tags}
+              url={photo.webformatURL}
+              id={photo.id}
+              key={photo.id}
+            />
           );
-      })}
+        })}
+      </div>
+      <LoadMore keyword={keyword} />
+      {target && <Carousel target={target} setTarget={setTarget} />}
     </div>
-    <LoadMore pageNum={pageNum} setPageNum={setPageNum}/>
-    {target && <Carousel target={target} setTarget={setTarget}/>}
-    </div>
-) 
-  : (
-    <div className="empty">No images were found.
-    Here's a cat
-    <img alt="cat-hug" src="https://i.imgur.com/MqGBqZs.gif" ></img>
+  ) : (
+    <div className="empty">
+      No images were found. Here's a cat
+      <img alt="cat-hug" src="https://i.imgur.com/MqGBqZs.gif"></img>
     </div>
   );
 };
