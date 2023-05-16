f1b8b83aee847562555cda68030f89e4e7571596
ETLDevs <etlbdevs@gmail.com>
2023-05-16 18:01:50 +0300

    like/unlike updates only favorites state

--- src/components/Like.js
+++ src/components/Like.js
@@ -1,26 +1,31 @@
 import { useContext } from "react";
 import { FavoritesContext } from "../contexts/FavoritesContext";
+import { PhotoContext } from "../contexts/PhotoContext";
 
 const Like = (props) => {
-const {id, title, url} = props;
-const { setFavorites, storage, setIds, ids} = useContext(FavoritesContext)
+  const { id, title, url } = props;
+  const { setFavorites, storage, ids } = useContext(FavoritesContext);
+  const { fetchPhotos, keyword } = useContext(PhotoContext);
 
-console.log(id)
+  return !ids.includes(id) ? (
+    <i
+      onClick={() => {
+        setFavorites([
+          ...JSON.parse(storage),
+          { id, webformatURL: url, tags: title },
+        ]);
+      }}
+      className="like fa-solid fa-heart"
+    ></i>
+  ) : (
+    <i
+      onClick={() => {
+        setFavorites((current) => current.filter((img) => img.id !== id));
+        if (keyword === "favorites") return fetchPhotos(keyword, 1);
+      }}
+      className="unlike fa-solid fa-heart"
+    ></i>
+  );
+};
 
-return !ids.includes(id) ? ( 
-
-        <i onClick={()=> {
-         setFavorites([...JSON.parse(storage), {id, webformatURL:url, tags:title}])
-         setIds(current => [...current, id])
-        }} className="like fa-solid fa-heart"></i>
-     ) :  ( 
-
-        <i onClick={()=> {
-         setFavorites(current => current.filter(img =>img.id !== id))
-         setIds(current => current.filter(ids => ids !== id))
-        }} className="unlike fa-solid fa-heart"></i>
-     );
-
-}
- 
 export default Like;
