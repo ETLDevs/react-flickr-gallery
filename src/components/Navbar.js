78647788682c2656c3f9e5aca13efc65189d108e
ETLDevs <etlbdevs@gmail.com>
2023-05-16 17:59:17 +0300

    add a favorites button to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,33 +1,39 @@
 import { useState } from "react";
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
 
-
 const Navbar = () => {
-  const [searchValue, setSearchValue] = useState('');
+  const [searchValue, setSearchValue] = useState("");
   return (
     <>
-    <nav className="nav">
-      <h1>Pixabay Gallery</h1>
-      <div className="search">
-      <div className="searchBar">
-        <i className="fa-solid fa-magnifying-glass"></i>
-        <input
-          type="text"
-          placeholder="Search Anything"
-          value={searchValue}
-          onChange={e => setSearchValue(e.target.value)}
-        />
-      </div>
-      <Link onClick={() => setSearchValue('')} to={`/search/${searchValue}`}> SEARCH</Link>
-      </div>
-      <div className="buttonGroup">
-      <Link to={'/'}>HOME</Link>
-        <Link to={'/birds'}>BIRDS</Link>
-        <Link to={`/sky`} >SKY</Link>
-        <Link to={'/mountain'} >MOUNTAIN</Link>
-        <Link to={'/food'} >FOOD</Link>
-      </div>
-    </nav>
+      <nav className="nav">
+        <h1>Pixabay Gallery</h1>
+        <div className="search">
+          <div className="searchBar">
+            <i className="fa-solid fa-magnifying-glass"></i>
+            <input
+              type="text"
+              placeholder="Search Anything"
+              value={searchValue}
+              onChange={(e) => setSearchValue(e.target.value)}
+            />
+          </div>
+          <Link
+            onClick={() => setSearchValue("")}
+            to={`/search/${searchValue}`}
+          >
+            {" "}
+            SEARCH
+          </Link>
+        </div>
+        <div className="buttonGroup">
+          <Link to={"/"}>HOME</Link>
+          <Link to={"/birds"}>BIRDS</Link>
+          <Link to={"/sky"}>SKY</Link>
+          <Link to={"/mountain"}>MOUNTAIN</Link>
+          <Link to={"/food"}>FOOD</Link>
+          <Link to={"/favorites"}>FAVORITES</Link>
+        </div>
+      </nav>
     </>
   );
 };
