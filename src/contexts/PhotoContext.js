691783a01c924e8a4c88c201f60253788052eb72
ETLDevs <etlbdevs@gmail.com>
2023-05-16 17:57:58 +0300

    fetch photos sends localStorage data

--- src/contexts/PhotoContext.js
+++ src/contexts/PhotoContext.js
@@ -1,34 +1,38 @@
-import { createContext, useState } from "react";
+import { createContext, useCallback, useState } from "react";
 import { API_KEY } from "../api/key";
 
 export const PhotoContext = createContext();
 
 const PhotoContextProvider = (props) => {
- 
   const [photos, setPhotos] = useState([]);
   const [ids, setIds] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  const fetchPhotos = (query, pageNum) => {
-    setIsLoading(true);
-    
-    const url = query === 'recent' ? `https://pixabay.com/api/?key=${API_KEY}&image_type=photo&page=${pageNum}&order=latest` : `https://pixabay.com/api/?key=${API_KEY}&q=${query}&image_type=photo&page=${pageNum}`;
-    
-    fetch(url)
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
 
+  const fetchPhotos = useCallback((query, pageNum) => {
+    setIsLoading(true);
+    if (query === "favorites") {
+      const storageFavorites = localStorage.getItem("favorites");
+      setIsLoading(false);
+      return setPhotos(storageFavorites ? JSON.parse(storageFavorites) : []);
+    }
+    const url =
+      query === "recent"
+        ? `https://pixabay.com/api/?key=${API_KEY}&image_type=photo&page=${pageNum}&order=latest&editors_choice=true`
+        : `https://pixabay.com/api/?key=${API_KEY}&q=${query}&image_type=photo&editors_choice=true&page=${pageNum}`;
+
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
         if (pageNum > 1) {
           data.hits.forEach((photo) => {
-            if (!ids.includes(photo.id)){
-             setPhotos((current) => [...current, photo]);}
+            if (!ids.includes(photo.id)) {
+              setPhotos((current) => [...current, photo]);
+            }
           });
-
-        } 
-        if(pageNum === 1) {
+        }
+        if (pageNum === 1) {
           setIds([]);
-          setPhotos( data.hits);
+          setPhotos(data.hits);
         }
         data.hits.forEach((photo) => {
           setIds((current) => [...current, photo.id]);
@@ -40,7 +44,7 @@ const PhotoContextProvider = (props) => {
         setIsLoading(false);
         console.error(err);
       });
-  };
+  }, []);
 
   return (
     <PhotoContext.Provider value={{ fetchPhotos, photos, isLoading }}>
